8b017eaa56ad118f1ee78765c492cb90b8337f38
Aaron Bushnell <aabushnell@gmail.com>
2025-10-07 13:04:00 +0200

    merge upstream main into dev-local

---
+++ apps/pallas_sync.cpp
@@ -0,0 +1,245 @@
+//
+// Created by Aaron Bushnell on 23.09.2025
+//
+
+#include <unistd.h>
+#include <cstdint>
+#include <cstdlib>
+#include "pallas/pallas.h"
+#include "pallas/pallas_archive.h"
+// #include "pallas/pallas_dbg.h"
+// #include "pallas/pallas_log.h"
+// #include "pallas/pallas_parameter_handler.h"
+// #include "pallas/pallas_read.h"
+#include "pallas/pallas_storage.h"
+// #include "pallas/pallas_write.h"
+
+#define DEBUG_LEVEL 2
+
+int main(int argc, char** argv) {
+
+  std::map<uint32_t, pallas::String> synced_strings;
+  std::map<uint32_t, uint32_t> string_ref_lookup;
+  uint32_t next_free_string_ref = 0;
+
+  std::map<uint32_t, pallas::Region> synced_regions;
+  std::map<uint32_t, uint32_t> region_ref_lookup;
+  uint32_t next_free_region_ref = 0;
+
+  char* trace_name = nullptr;
+
+  if (argc < 2) {
+    std::cout << "ERROR: Missing trace file" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  trace_name = argv[1];
+  pallas::GlobalArchive* trace = pallas_open_trace(trace_name);
+  
+  std::cout << "Pallas: Trace File Opened" << std::endl;
+
+  // loop over StringRef -> String map in GlobalArchive Definition
+  for (auto const& [string_ref, string]
+    : trace->definitions.strings) {
+    if (DEBUG_LEVEL > 0) {
+      std::cout << "Pallas: string #" << string_ref;
+      std::cout << " = '" << string.str << "'" << std::endl;
+    }
+
+    // loop over local synchronized StringRef -> String map
+    bool contains_string = false;
+    for (auto const& [synced_string_ref, synced_string]
+      : synced_strings) {
+      if (std::strcmp(string.str, synced_string.str) == 0) {
+        contains_string = true;
+
+        // record updated StringRef
+        string_ref_lookup[string_ref] = (uint32_t) synced_string_ref;
+      }
+    }
+
+    if (!contains_string) {
+      // add new String to synchronized map
+      auto& s = synced_strings[next_free_string_ref];
+      s.string_ref = next_free_string_ref;
+      s.length = string.length;
+      s.str = (char*) std::calloc(sizeof(char), s.length);
+      std::strncpy(s.str, string.str, s.length);
+
+      // record new StringRef and increment id
+      string_ref_lookup[string_ref] = (uint32_t) next_free_string_ref;
+      next_free_string_ref++;
+    }
+  }
+  
+  // loop over RegionRef -> Region map in GlobalArchive Definition
+  for (auto const& [region_ref, region]
+    : trace->definitions.regions) {
+    if (DEBUG_LEVEL > 0) {
+      std::cout << "Pallas: region #" << region_ref;
+      std::cout << " = (" << region.string_ref << ")'";
+      std::cout << trace->definitions.strings[region.string_ref].str;
+      std::cout << "'" << std::endl;
+    }
+
+    // loop over local synchronized RegionRef -> Region map
+    bool contains_region = false;
+    for (auto const& [synced_region_ref, synced_region]
+      : synced_regions) {
+      if (string_ref_lookup[region.string_ref] == synced_region.string_ref) {
+        contains_region = true;
+
+        // record updated RegionRef
+        region_ref_lookup[region_ref] = (uint32_t) synced_region_ref;
+      }
+    }
+
+    if (!contains_region) {
+      // add new Region to synchronized map
+      auto& r = synced_regions[next_free_region_ref];
+      r.region_ref = next_free_region_ref;
+      r.string_ref = string_ref_lookup[region.string_ref];
+
+      // record new RegionRef and increment id
+      region_ref_lookup[region_ref] = (uint32_t) next_free_region_ref;
+      next_free_region_ref++;
+    }
+
+  }
+
+  // add synchronized string + region maps to Definition
+  trace->definitions.strings = std::move(synced_strings);
+  trace->definitions.regions = std::move(synced_regions);
+
+  // update other Definition elements' StringRefs
+  // attributes
+  for (auto& [attribute_ref, attribute]
+    : trace->definitions.attributes) {
+    attribute.name = string_ref_lookup[attribute.name];
+    attribute.description = string_ref_lookup[attribute.description];
+  }
+
+  // groups
+  for (auto& [group_ref, group]
+    : trace->definitions.groups) {
+    group.name = string_ref_lookup[group.name];
+  }
+
+  // comms
+  for (auto& [comm_ref, comm]
+    : trace->definitions.comms) {
+    comm.name = string_ref_lookup[comm.name];
+  }
+
+  // update location_groups and location StringRefs and write new data
+  auto newDirName = strdup((
+      std::string(trace->dir_name) + "_synchronized"
+  ).c_str());
+
+  for (auto& lg : trace->location_groups) {
+    if (DEBUG_LEVEL > 0) {
+      std::cout << "Reading archive " << lg.id << " @ " << trace->dir_name << std::endl;
+    }
+    lg.name = string_ref_lookup[lg.name];
+    auto* a = trace->getArchive(lg.id);
+    for (auto& loc : a->locations) {
+      if (DEBUG_LEVEL > 0) {
+        std::cout << "\tReading thread " << loc.id << " @ " << a->dir_name << std::endl;
+      }
+      loc.name = string_ref_lookup[loc.name];
+      auto* t = a->getThread(loc.id);
+      if (DEBUG_LEVEL > 0) {
+        std::cout << "\tWriting thread " << t->id << " @ " << a->dir_name << std::endl;
+      }
+      t->store(newDirName, trace->parameter_handler, true);
+      a->freeThread(loc.id);
+    }
+    if (DEBUG_LEVEL > 0) {
+      std::cout << "Writing archive " << lg.id << " @ " << trace->dir_name << std::endl;
+    }
+    a->store(newDirName, trace->parameter_handler);
+    a->dir_name = nullptr;
+    trace->freeArchive(lg.id);
+  }
+
+  // write updated GlobalArchive
+  trace->store(newDirName, trace->parameter_handler);
+
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Synced string list" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [string_ref, string] 
+      : synced_strings) {
+      std::cout << "Pallas: string #" << string.string_ref;
+      std::cout << " = '" << string.str << "'" << std::endl;
+    }
+  }
+
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Definition string list" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [string_ref, string] 
+      : trace->definitions.strings) {
+      std::cout << "Pallas: string #" << string.string_ref;
+      std::cout << " = '" << string.str << "'" << std::endl;
+    }
+  }
+  
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Synced string ref lookup " << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [old_ref, new_ref] 
+      : string_ref_lookup) {
+      std::cout << "Mapped ref #" << old_ref << " -> ";
+      std::cout << new_ref << std::endl;
+    }
+  }
+
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Synced region list " << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [region_ref, region]
+      : synced_regions) {
+      std::cout << "Pallas: region #" << region_ref;
+      std::cout << " = (" << region.string_ref << ")'";
+      std::cout << synced_strings[region.string_ref].str;
+      std::cout << "'" << std::endl;
+    }
+  }
+
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Definition region list " << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [region_ref, region]
+      : trace->definitions.regions) {
+      std::cout << "Pallas: region #" << region_ref;
+      std::cout << " = (" << region.string_ref << ")'";
+      std::cout << trace->definitions.strings[region.string_ref].str;
+      std::cout << "'" << std::endl;
+    }
+  }
+
+  if (DEBUG_LEVEL > 1) {
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Synced region ref lookup " << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+
+    for (auto const& [old_ref, new_ref] 
+      : region_ref_lookup) {
+      std::cout << "Mapped ref #" << old_ref << " -> ";
+      std::cout << new_ref << std::endl;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
